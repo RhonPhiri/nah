27dd17032cde5c0e9d64df41d4a8048bed3b497c
RhonPhiri <166160995+RhonPhiri@users.noreply.github.com>
2025-11-22 17:57:26 +0200

    Added the hymnbookmark, a representation of the hymnthat has been added to a collection

---
+++ nah/lib/data/domain/models/hymn_bookmark/hymn_bookmark.dart
@@ -0,0 +1,22 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'hymn_bookmark.freezed.dart';
+
+part 'hymn_bookmark.g.dart';
+
+/// [HymnBookmark] is a class of a representation of a bookmark assigned to a colleciton
+
+@freezed
+abstract class HymnBookmark with _$HymnBookmark {
+  const factory HymnBookmark({
+    // Unique identifier that is equal to the hymn id.
+    required int id,
+    // Title of the hymn that has been bookmarked.
+    required String title,
+    // Identifier of the collection to which the hymn has been bookmarked.
+    required int hymnCollectionId,
+  }) = _HymnBookmark;
+
+  factory HymnBookmark.fromJson(Map<String, Object?> json) =>
+      _$HymnBookmarkFromJson(json);
+}
