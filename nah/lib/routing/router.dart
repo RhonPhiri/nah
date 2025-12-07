0d426e4a66700bdf9a522ef29e513f934ed6a334
RhonPhiri <166160995+RhonPhiri@users.noreply.github.com>
2025-12-07 13:36:18 +0200

    Added the different screens and pages

--- nah/lib/routing/router.dart
+++ nah/lib/routing/router.dart
@@ -3,6 +3,7 @@ import 'package:nah/routing/routes.dart';
 import 'package:nah/ui/about/widgets/about_page.dart';
 import 'package:nah/ui/home/widgets/home_screen.dart';
 import 'package:nah/ui/hymn_collections/widgets/hymn_collection_page.dart';
+import 'package:nah/ui/hymn_details/widgets/details_screen.dart';
 import 'package:nah/ui/hymnals/widgets/hymnal_screen.dart';
 import 'package:nah/ui/hymns/widgets/hymn_page.dart';
 import 'package:provider/provider.dart';
@@ -16,14 +17,27 @@ GoRouter get router {
         builder: (context, state) {
           return HomeScreen(
             viewModel: context.read(),
-            pages: [HymnPage(), HymnCollectionPage(), AboutPage()],
+            pages: [
+              HymnPage(
+                secondScreen: DetailsScreen(),
+                thirdScreen: HymnalScreen(viewModel: context.read()),
+              ),
+              HymnCollectionPage(),
+              AboutPage(),
+            ],
           );
         },
         routes: [
           GoRoute(
             path: Routes.hymnals,
             builder: (context, state) {
-              return HymnalScreen();
+              return HymnalScreen(viewModel: context.read());
+            },
+          ),
+          GoRoute(
+            path: Routes.details,
+            builder: (context, state) {
+              return DetailsScreen();
             },
           ),
         ],
