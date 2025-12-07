7f0e178bb1329d168d9b33a8a81740e3688b1566
RhonPhiri <166160995+RhonPhiri@users.noreply.github.com>
2025-12-07 13:39:00 +0200

    Added the implementation to render the hymnal ui

--- nah/lib/ui/hymnals/widgets/hymnal_screen.dart
+++ nah/lib/ui/hymnals/widgets/hymnal_screen.dart
@@ -1,14 +1,87 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:nah/routing/routes.dart';
+import 'package:nah/ui/core/ui/error_button.dart';
+import 'package:nah/ui/hymnals/view_model/hymnal_view_model.dart';
 
-class HymnalScreen extends StatelessWidget {
-  const HymnalScreen({super.key});
+class HymnalScreen extends StatefulWidget {
+  const HymnalScreen({super.key, required this.viewModel});
+  final HymnalViewModel viewModel;
+
+  @override
+  State<HymnalScreen> createState() => _HymnalScreenState();
+}
+
+class _HymnalScreenState extends State<HymnalScreen> {
+  @override
+  void initState() {
+    super.initState();
+    widget.viewModel.selectHymnalId.addListener(_onHymnalSelected);
+  }
+
+  @override
+  void dispose() {
+    widget.viewModel.selectHymnalId.removeListener(_onHymnalSelected);
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: CustomScrollView(
-        slivers: [SliverAppBar.medium(title: Text("H Y M N A L  S C R E E N"))],
+    return PopScope(
+      onPopInvokedWithResult: (didPop, result) {
+        if (didPop) context.go(Routes.home);
+      },
+      child: Scaffold(
+        body: ListenableBuilder(
+          listenable: widget.viewModel.load,
+          builder: (context, child) {
+            if (widget.viewModel.load.running) {
+              return CircularProgressIndicator();
+            }
+            if (widget.viewModel.load.error) {
+              return ErrorButton(text: "Error while loading hymnals");
+            }
+            return child!;
+          },
+          child: ListenableBuilder(
+            listenable: widget.viewModel,
+            builder: (context, _) {
+              final hymnals = widget.viewModel.hymnals;
+              return CustomScrollView(
+                slivers: [
+                  SliverAppBar.medium(title: Text("Hymnals")),
+                  SliverList.builder(
+                    itemCount: hymnals.length,
+                    itemBuilder: (context, index) {
+                      final hymnal = hymnals[index];
+                      return ListTile(
+                        key: ValueKey('hymnalListTile_$index'),
+                        leading: Icon(
+                          (widget.viewModel.selectedHymnalId == index)
+                              ? Icons.book_rounded
+                              : Icons.book_outlined,
+                          size: 48,
+                        ),
+                        title: Text(hymnal.title),
+                        subtitle: Text(hymnal.language),
+                        onTap: () async => await widget.viewModel.selectHymnalId
+                            .execute(index),
+                      );
+                    },
+                  ),
+                ],
+              );
+            },
+          ),
+        ),
       ),
     );
+  }
+
+  void _onHymnalSelected() {
+    if (widget.viewModel.selectHymnalId.completed) {
+      widget.viewModel.selectHymnalId.clearResult();
+      context.pop();
+    }
   }
 }
