17696c53672c8e3792ff3c7a73d7a003ad7abc63
RhonPhiri <166160995+RhonPhiri@users.noreply.github.com>
2025-12-07 13:39:37 +0200

    Added the basic widgets, and switches to allow adaptiveness and responsiveness

--- nah/lib/ui/hymns/widgets/hymn_page.dart
+++ nah/lib/ui/hymns/widgets/hymn_page.dart
@@ -1,32 +1,68 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:nah/routing/routes.dart';
+import 'package:nah/ui/core/theme/dimens.dart';
+import 'package:nah/ui/hymn_details/widgets/details_screen.dart';
+import 'package:nah/ui/hymnals/widgets/hymnal_screen.dart';
 
 class HymnPage extends StatelessWidget {
-  const HymnPage({super.key});
+  const HymnPage({
+    super.key,
+    required this.secondScreen,
+    required this.thirdScreen,
+  });
+  final Widget secondScreen;
+  final Widget thirdScreen;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: CustomScrollView(
-        slivers: [
-          SliverAppBar.medium(
-            title: Text("Hymnal Title"),
-            scrolledUnderElevation: 0,
-            elevation: 0,
-            actions: [
-              IconButton(onPressed: () {}, icon: Icon(Icons.search)),
+      body: Row(
+        children: [
+          Flexible(
+            flex: 2,
+            child: CustomScrollView(
+              slivers: [
+                SliverAppBar.medium(
+                  title: Text("Hymnal Title"),
+                  scrolledUnderElevation: 0,
+                  elevation: 0,
+                  actions: [
+                    IconButton(onPressed: () {}, icon: Icon(Icons.search)),
 
-              IconButton(
-                onPressed: () => context.go("/hymnals"),
-                icon: Icon(Icons.menu_book),
-              ),
-            ],
+                    !Dimens(context).isExtraLarge
+                        ? IconButton(
+                            onPressed: () =>
+                                context.go(Routes.relativeHymnalsPath),
+                            icon: Icon(Icons.menu_book),
+                          )
+                        : SizedBox.shrink(),
+                  ],
+                ),
+                SliverList.builder(
+                  itemCount: 50,
+                  itemBuilder: (context, index) => ListTile(
+                    title: Text("Tile"),
+                    onTap: () {
+                      context.go(Routes.relativeDetails);
+                    },
+                  ),
+                ),
+              ],
+            ),
           ),
-          SliverList.builder(
-            itemCount: 50,
-            itemBuilder: (context, index) =>
-                ListTile(title: Text("Tile"), onTap: () {}),
-          ),
+          !(Dimens(context).isCompact || Dimens(context).isMedium)
+              ? Flexible(
+                  flex: 3,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 24.0),
+                    child: secondScreen,
+                  ),
+                )
+              : SizedBox.shrink(),
+          Dimens(context).isExtraLarge
+              ? Flexible(flex: 2, child: thirdScreen)
+              : SizedBox.shrink(),
         ],
       ),
     );
