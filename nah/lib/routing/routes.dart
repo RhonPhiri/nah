7b50ca9f0c10bc1fa9bf32600d4916eeb5d9b59c
RhonPhiri <166160995+RhonPhiri@users.noreply.github.com>
2025-12-07 13:36:52 +0200

    Added the paths & relative paths to manage the routing

--- nah/lib/routing/routes.dart
+++ nah/lib/routing/routes.dart
@@ -1,4 +1,7 @@
 abstract final class Routes {
   static const home = "/";
   static const hymnals = "hymnals";
+  static const relativeHymnalsPath = "/$hymnals";
+  static const details = "details";
+  static const relativeDetails = "/$details";
 }
