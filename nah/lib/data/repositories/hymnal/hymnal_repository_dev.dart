5cd1cff8b3b46e6df8f354bbdffadcb07ae7b896
RhonPhiri <166160995+RhonPhiri@users.noreply.github.com>
2025-12-07 13:31:14 +0200

    Implemented the abstract methods

--- nah/lib/data/repositories/hymnal/hymnal_repository_dev.dart
+++ nah/lib/data/repositories/hymnal/hymnal_repository_dev.dart
@@ -1,3 +1,4 @@
+import 'package:nah/data/services/shared_pref_service.dart';
 import 'package:nah/domain/models/hymnal/hymnal.dart';
 import 'package:nah/data/repositories/hymnal/hymnal_repository.dart';
 import 'package:nah/data/services/data_service.dart';
@@ -5,9 +6,13 @@ import 'package:nah/utils/result.dart';
 
 class HymnalRepositoryDev implements HymnalRepository {
   final DataService _dataService;
+  final SharedPrefService _prefs;
 
-  HymnalRepositoryDev({required DataService dataService})
-    : _dataService = dataService;
+  HymnalRepositoryDev({
+    required DataService dataService,
+    required SharedPrefService prefs,
+  }) : _dataService = dataService,
+       _prefs = prefs;
 
   @override
   Future<Result<List<Hymnal>>> getHymnals() async {
@@ -16,5 +21,28 @@ class HymnalRepositoryDev implements HymnalRepository {
       return Result.success(result.data);
     }
     return Result.error((result as Error<List<Hymnal>>).error);
+  }
+
+  @override
+  Future<Result<int?>> getStoredHymnalId() async {
+    try {
+      final result = await _prefs.fetchHymnalId();
+      if (result is Success<int?>) {
+        return Result.success(result.data);
+      }
+      return Result.error((result as Error).error);
+    } catch (e) {
+      return Result.error(Exception(e));
+    }
+  }
+
+  @override
+  Future<Result<void>> storeSelectedHymnalId(int hymnalId) async {
+    try {
+      await _prefs.saveHymnalId(hymnalId);
+      return Result.success(null);
+    } on Exception catch (e) {
+      return Result.error(Exception(e));
+    }
   }
 }
