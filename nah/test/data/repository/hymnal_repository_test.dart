0a66f8fef8e61c176728406cf07d8065b8d10dff
RhonPhiri <166160995+RhonPhiri@users.noreply.github.com>
2025-12-07 15:38:44 +0200

    Added the test for the methods added

--- nah/test/data/repository/hymnal_repository_test.dart
+++ nah/test/data/repository/hymnal_repository_test.dart
@@ -2,18 +2,26 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:nah/data/repositories/hymnal/hymnal_repository.dart';
 import 'package:nah/data/repositories/hymnal/hymnal_repository_dev.dart';
 import 'package:nah/data/services/data_service.dart';
+import 'package:nah/data/services/shared_pref_service.dart';
 import 'package:nah/utils/result.dart';
 import '../../../testing/fakes/data/service/fake_data_service.dart';
+import '../../../testing/fakes/data/service/fake_shared_preference_service.dart';
+import '../../../testing/fakes/domain/models/hymnal.dart';
 import '../../../testing/utils/result.dart';
 
 void main() {
   group('HymnalRepository tests', () {
     late DataService dataService;
     late HymnalRepository hymnalRepository;
+    late SharedPrefService sharedPrefs;
 
     setUpAll(() {
       dataService = FakeDataService();
-      hymnalRepository = HymnalRepositoryDev(dataService: dataService);
+      sharedPrefs = FakeSharedPreferenceService();
+      hymnalRepository = HymnalRepositoryDev(
+        dataService: dataService,
+        prefs: sharedPrefs,
+      );
     });
 
     test('should get hymnals', () async {
@@ -22,6 +30,19 @@ void main() {
       expect(result, isA<Success>());
       expect(result.asSuccess.data.length, 1);
       expect(result.asSuccess.data.first.language, "LANGUAGE");
+    });
+
+    test('should store the selected hymnal Id', () async {
+      final result = await hymnalRepository.storeSelectedHymnalId(kHymnal.id);
+
+      expect(result, isA<Success>());
+    });
+
+    test('should get the previously stored hymnal id', () async {
+      final result = await hymnalRepository.getStoredHymnalId();
+
+      expect(result, isA<Success>());
+      expect(result.asSuccess.data, kHymnal.id);
     });
   });
 }
