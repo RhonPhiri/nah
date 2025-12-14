5a8764512bbeace2f7914f84d6ddd693dff253d8
RhonPhiri <166160995+RhonPhiri@users.noreply.github.com>
2025-12-14 11:29:33 +0200

    Added the fetchHymnal & saveHymnal methods to the sharedPreferences

--- nah/lib/data/services/shared_pref_service.dart
+++ nah/lib/data/services/shared_pref_service.dart
@@ -5,13 +5,40 @@ import 'package:shared_preferences/shared_preferences.dart';
 class SharedPrefService {
   final _log = Logger("SHARED_PREFERENCES");
 
-  static const _hymnalIdKey = "HYMNALID";
+  static const _hymnalKey = "HYMNALID";
 
-  Future<Result<int?>> fetchHymnalId() async {
+  static const _usageStatusKey = "USAGE";
+
+  Future<Result<String?>> fetchHymnal() async {
     try {
       final prefs = await SharedPreferences.getInstance();
-      _log.fine("Accessing shared preferences successful: getting hymnal id");
-      return Result.success(prefs.getInt(_hymnalIdKey));
+      _log.fine("Accessing shared preferences successful: getting hymnal");
+      return Result.success(prefs.getString(_hymnalKey));
+    } on Exception catch (e) {
+      _log.warning("Failed to access the shared preferences to get hymnal", e);
+      return Result.error(Exception(e));
+    }
+  }
+
+  Future<Result<void>> saveHymnal(String hymnal) async {
+    try {
+      final prefs = await SharedPreferences.getInstance();
+      _log.fine("Accessing shared preferences successful: setting hymnal");
+      await prefs.setString(_hymnalKey, hymnal);
+      return Result.success(null);
+    } on Exception catch (e) {
+      _log.warning("Failed to save the current selected hymnal");
+      return Result.error(Exception(e));
+    }
+  }
+
+  Future<Result<bool?>> fetchUsageState() async {
+    try {
+      final prefs = await SharedPreferences.getInstance();
+      _log.fine(
+        "Accessing shared preferences successful: getting usage status",
+      );
+      return Result.success(prefs.getBool(_usageStatusKey));
     } on Exception catch (e) {
       _log.warning(
         "Failed to access the shared preferences to get hymnal id",
@@ -21,14 +48,14 @@ class SharedPrefService {
     }
   }
 
-  Future<Result<void>> saveHymnalId(int hymnalId) async {
+  Future<Result<void>> saveUsageStatus() async {
     try {
       final prefs = await SharedPreferences.getInstance();
-      _log.fine("Accessing shared preferences successful: setting hymnalId");
-      await prefs.setInt(_hymnalIdKey, hymnalId);
+      _log.fine("Accessing shared preferences successful: setting usageStatus");
+      await prefs.setBool(_usageStatusKey, false);
       return Result.success(null);
     } on Exception catch (e) {
-      _log.warning("Failed to save the current selected hymnal id");
+      _log.warning("Failed to set the usage status of the user");
       return Result.error(Exception(e));
     }
   }
