f856a59145f7b79fc38c9bc747580b37cb43bcdd
RhonPhiri <166160995+RhonPhiri@users.noreply.github.com>
2025-12-14 11:32:05 +0200

    Added the onBoarding route for the onboarding hymnal screen

--- nah/lib/routing/routes.dart
+++ nah/lib/routing/routes.dart
@@ -2,6 +2,7 @@ abstract final class Routes {
   static const home = "/";
   static const hymnals = "hymnals";
   static const relativeHymnalsPath = "/$hymnals";
+  static const onBoarding = "/onBoarding";
   static const details = "details";
   static const relativeDetails = "/$details";
 }
