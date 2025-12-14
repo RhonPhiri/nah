b5989700aa797d3bd13bd61f7e12dd9f22532f8e
RhonPhiri <166160995+RhonPhiri@users.noreply.github.com>
2025-12-14 11:31:21 +0200

    Added the new methods for mocking

--- nah/testing/fakes/data/service/fake_shared_preference_service.dart
+++ nah/testing/fakes/data/service/fake_shared_preference_service.dart
@@ -1,16 +1,29 @@
 import 'package:nah/data/services/shared_pref_service.dart';
 import 'package:nah/utils/result.dart';
 
-class FakeSharedPreferenceService implements SharedPrefService {
-  int? hymnalId;
+class FakeSharedPreferenceService extends SharedPrefService {
+  String? hymnal;
+  bool? usageStatus;
+
   @override
-  Future<Result<int?>> fetchHymnalId() async {
-    return Result.success(hymnalId);
+  Future<Result<String?>> fetchHymnal() async {
+    return Result.success(hymnal);
   }
 
   @override
-  Future<Result<void>> saveHymnalId(int hymnalId) async {
-    this.hymnalId = hymnalId;
+  Future<Result<void>> saveHymnal(String hymnal) async {
+    this.hymnal = hymnal;
+    return Result.success(null);
+  }
+
+  @override
+  Future<Result<bool?>> fetchUsageState() async {
+    return Result.success(usageStatus);
+  }
+
+  @override
+  Future<Result<void>> saveUsageStatus() async {
+    usageStatus = false;
     return Result.success(null);
   }
 }
