fc5dd0af8dfb304cc3e4f26daf52bbd04ae7c2d2
RhonPhiri <166160995+RhonPhiri@users.noreply.github.com>
2025-12-24 10:18:41 +0200

    Removed the comments

--- nah/testing/utils/result.dart
+++ nah/testing/utils/result.dart
@@ -1,9 +1,7 @@
 import 'package:nah/utils/result.dart';
 
 extension ResultCast<T> on Result<T> {
-  /// Convenience method to cast to Ok
   Success<T> get asSuccess => this as Success<T>;
 
-  /// Convenience method to cast to Error
   Error get asError => this as Error<T>;
 }
