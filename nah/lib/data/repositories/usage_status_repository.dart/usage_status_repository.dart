d3387f94620f73aee6b7765f09fb4bd616f10243
RhonPhiri <166160995+RhonPhiri@users.noreply.github.com>
2025-12-24 10:24:13 +0200

    Made some deletions and pub updates

--- nah/lib/data/repositories/usage_status_repository.dart/usage_status_repository.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/foundation.dart';
-
-/// [UsageStatusRepository] manages the data that will determine whether the hymnal screen should be shown
-/// at startup or not.
-abstract class UsageStatusRepository extends ChangeNotifier {
-  Future<bool> get isFirstTimeUser;
-
-  Future<void> enterApp();
-}
