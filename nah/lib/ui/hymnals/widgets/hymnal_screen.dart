d048ab835534b19a369c78ea12fd9dcd9a5d6434
RhonPhiri <166160995+RhonPhiri@users.noreply.github.com>
2025-12-24 10:17:01 +0200

    Added the pop scope functionality

--- nah/lib/ui/hymnals/widgets/hymnal_screen.dart
+++ nah/lib/ui/hymnals/widgets/hymnal_screen.dart
@@ -1,19 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:nah/data/repositories/usage_status_repository.dart/usage_status_repository.dart';
 import 'package:nah/ui/core/ui/error_button.dart';
-import 'package:nah/ui/hymnals/view_model/hymnal_view_model.dart';
-import 'package:provider/provider.dart';
+import 'package:nah/ui/hymnals/viewmodel/hymnal_view_model.dart';
 
 class HymnalScreen extends StatefulWidget {
-  const HymnalScreen({
-    super.key,
-    required this.viewModel,
-    this.isOnboarding = false,
-  });
+  const HymnalScreen({super.key, required this.viewModel});
   final HymnalViewModel viewModel;
-
-  final bool isOnboarding;
 
   @override
   State<HymnalScreen> createState() => _HymnalScreenState();
@@ -34,56 +26,59 @@ class _HymnalScreenState extends State<HymnalScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: ListenableBuilder(
-        listenable: widget.viewModel.load,
-        builder: (context, child) {
-          if (widget.viewModel.load.running) {
-            return const Center(child: CircularProgressIndicator());
-          }
-          if (widget.viewModel.load.error) {
-            return ErrorButton(text: "Error while loading hymnals");
-          }
-          return child!;
-        },
-        child: ListenableBuilder(
-          listenable: widget.viewModel,
-          builder: (context, _) {
-            final hymnals = widget.viewModel.hymnals;
-            return CustomScrollView(
-              slivers: [
-                SliverAppBar.medium(
-                  title: Text(
-                    widget.isOnboarding ? "Select a hymnal" : "Hymnals",
-                  ),
-                ),
-                SliverList.builder(
-                  itemCount: hymnals.length,
-                  itemBuilder: (context, index) {
-                    final hymnal = hymnals[index];
-
-                    return Material(
-                      color: Theme.of(context).cardColor,
-                      clipBehavior: Clip.hardEdge,
-                      child: ListTile(
-                        key: ValueKey('hymnalListTile_$index'),
-                        leading: Icon(
-                          (widget.viewModel.selectedHymnal?.id == hymnal.id)
-                              ? Icons.book_rounded
-                              : Icons.book_outlined,
-                          size: 48,
-                        ),
-                        title: Text(hymnal.title),
-                        subtitle: Text(hymnal.language),
-                        onTap: () =>
-                            widget.viewModel.selectHymnal.execute(hymnal),
-                      ),
-                    );
-                  },
-                ),
-              ],
-            );
+    return PopScope(
+      canPop: true,
+      child: Scaffold(
+        body: ListenableBuilder(
+          listenable: widget.viewModel.load,
+          builder: (context, child) {
+            if (widget.viewModel.load.running) {
+              return const Center(child: CircularProgressIndicator());
+            }
+            if (widget.viewModel.load.error) {
+              return ErrorButton(
+                text: "Error while loading hymnals",
+                onPressed: () => context.go("/"),
+              );
+            }
+            return child!;
           },
+          child: ListenableBuilder(
+            listenable: widget.viewModel,
+            builder: (context, _) {
+              final hymnals = widget.viewModel.hymnals;
+              return CustomScrollView(
+                slivers: [
+                  SliverAppBar.medium(title: Text("Hymnals")),
+                  SliverList.builder(
+                    itemCount: hymnals.length,
+                    itemBuilder: (context, index) {
+                      final hymnal = hymnals[index];
+
+                      return Material(
+                        color: Theme.of(context).cardColor,
+                        clipBehavior: Clip.hardEdge,
+                        child: ListTile(
+                          key: ValueKey('hymnalListTile_$index'),
+                          splashColor: Colors.transparent,
+                          leading: Icon(
+                            (widget.viewModel.selectedHymnal?.id == hymnal.id)
+                                ? Icons.book_rounded
+                                : Icons.book_outlined,
+                            size: 48,
+                          ),
+                          title: Text(hymnal.title),
+                          subtitle: Text(hymnal.language),
+                          onTap: () =>
+                              widget.viewModel.selectHymnal.execute(hymnal),
+                        ),
+                      );
+                    },
+                  ),
+                ],
+              );
+            },
+          ),
         ),
       ),
     );
@@ -91,12 +86,8 @@ class _HymnalScreenState extends State<HymnalScreen> {
 
   void _onHymnalSelected() async {
     if (widget.viewModel.selectHymnal.completed) {
-      if (widget.isOnboarding) {
-        context.read<UsageStatusRepository>().enterApp();
-        return;
-      }
       widget.viewModel.selectHymnal.clearResult();
-      context.pop();
+      if (mounted) context.pop(widget.viewModel.selectedHymnal);
     }
   }
 }
